f2c93fa6c304d91ba3e17d87baedd2c8edd27935
Carlos Ariel SB <arielsb.ti19@utsjr.edu.mx>
2022-05-27 16:42:34 -0500

    Envio V.1.3

--- app/src/main/java/com/example/loginapp/Register.java
+++ app/src/main/java/com/example/loginapp/Register.java
@@ -2,13 +2,31 @@ package com.example.loginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        textView = findViewById(R.id.textView5);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this, Login.class);
+                startActivity(intent);
+
+                Toast.makeText(Register.this, "You clicked on text",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
